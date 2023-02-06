94b1e3f41794ddc3fbb6cfaed4110c0e2fc80b15
KMinh <daukhacminh006@gmail.com>
2023-02-06 21:53:37 +0700

    Initial Commit

---
+++ openai-config.js
@@ -0,0 +1,7 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  organization: "org-ahdlKsrK8suAT3yPTJYeEQ7A",
+  apiKey: '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',
+});
+export const openai = new OpenAIApi(configuration)
