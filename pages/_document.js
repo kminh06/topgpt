443725d8a87c9583ab6903e0afc5cc9b52dc0486
KMinh <daukhacminh006@gmail.com>
2023-06-24 15:04:30 +0700

    Adsense code

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6145213958807273" crossOrigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
